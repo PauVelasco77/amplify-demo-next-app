1181041e2903e86f9033bc9c6e9bddd0f77683e8
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-10-03 13:08:23 +0200

    remove Amplify config from AWSAuthenticator component

--- src/app/components/AWSAuthenticator.jsx
+++ src/app/components/AWSAuthenticator.jsx
@@ -3,16 +3,6 @@
 import {Authenticator} from "@aws-amplify/ui-react";
 import {Amplify} from "aws-amplify";
 
-Amplify.configure({
-  Auth: {
-    mandatorySignId: true,
-    region: "eu-west-3",
-    userPoolId: "eu-west-3_2rn7vUqE6",
-    userPoolWebClientId: "5q6fu1vdu9amke658k9p4ijgc7",
-  },
-  ssr: true,
-});
-
 export default function AWSAuthenticator() {
   return (
     <Authenticator>
