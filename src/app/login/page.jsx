ad999cff72b4bdd107213dca53a7a1e55c0c6dd6
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-10-03 13:02:38 +0200

    change login page to client page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,26 +1,39 @@
-import {headers} from "next/headers";
-import {withSSRContext} from "aws-amplify";
-import {redirect} from "next/navigation";
+"use client";
+
+import {Suspense, useEffect, useState} from "react";
 import AWSAuthenticator from "../components/AWSAuthenticator";
+import {useRouter} from "next/navigation";
+import {Auth} from "aws-amplify";
 
-export default async function Login() {
-  const req = {
-    headers: {
-      cookie: headers().get("cookie"),
-    },
-  };
+export default function Login() {
+  const [isLogged, setIsLogged] = useState(false);
 
-  const {Auth} = withSSRContext({req});
-
-  try {
-    const user = await Auth.currentAuthenticatedUser();
-    redirect("/");
-  } catch (error) {
-    return (
-      <main className="flex flex-col items-center justify-center w-full h-full">
-        <h1>LOGIN</h1>
-        <AWSAuthenticator />
-      </main>
-    );
+  const route = useRouter();
+  async function isUserAuthenticated() {
+    try {
+      await Auth.currentAuthenticatedUser();
+      console.log("User is authenticated");
+      route.push("/");
+    } catch (error) {
+      setIsLogged(false);
+    }
   }
+
+  useEffect(() => {
+    console.log("HOLA");
+    isUserAuthenticated();
+  }, [isUserAuthenticated, setIsLogged]);
+
+  return (
+    <main className="flex flex-col items-center justify-center w-full h-full">
+      <Suspense fallback={<h1>LOADING!!!!</h1>}>
+        {isLogged && (
+          <div>
+            <h1>LOGIN</h1>
+            <AWSAuthenticator />
+          </div>
+        )}
+      </Suspense>
+    </main>
+  );
 }
