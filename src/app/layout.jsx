666bd64ace13afe0d1b560936b2c57243531d647
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-10-06 16:20:58 +0200

    change styles loading order

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import ConfigureAmplifyClientSide from "./components/ConfigureAmplifyClientSide";
-import "@aws-amplify/ui-react/styles.css";
 import "./globals.css";
+import "@aws-amplify/ui-react/styles.css";
 import LogoutButton from "./components/LogoutButton";
 import {Amplify} from "aws-amplify";
 import {awsExports} from "../../awsExports";
