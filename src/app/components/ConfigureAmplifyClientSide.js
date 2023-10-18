ed0c17c219f17d650e776608f7b6d78abab02932
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-10-18 15:36:46 +0200

    remove unused variables

--- src/app/components/ConfigureAmplifyClientSide.js
+++ src/app/components/ConfigureAmplifyClientSide.js
@@ -3,11 +3,6 @@
 import {Amplify} from "aws-amplify";
 import {awsExports} from "../../../awsExports";
 
-const region = process.env.NEXT_PUBLIC_AWS_REGION ?? "";
-const userPoolId = process.env.NEXT_PUBLIC_AWS_USER_POOL_ID ?? "";
-const userPoolWebClientId =
-  process.env.NEXT_PUBLIC_AWS_USER_POOL_WEB_CLIENT_ID ?? "";
-
 Amplify.configure({
   ...awsExports,
   ssr: true,
